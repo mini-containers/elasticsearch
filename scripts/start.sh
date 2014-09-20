2f9909108da0e789daa2d6bee66f535d2f17eca0
Luis Lavena <luislavena@gmail.com>
2014-09-20 11:18:43 -0300

    reindent

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,21 +3,21 @@
 local ELASTICSEARCH_YML="/opt/elasticsearch/config/elasticsearch.yml"
 
 shutdown() {
-  echo "=> Shutdown requested, stopping Elasticsearch..."
+	echo "=> Shutdown requested, stopping Elasticsearch..."
 
-  curl --silent -XPOST 'http://localhost:9200/_cluster/nodes/_local/_shutdown' > /dev/null
-  sleep 4
+	curl --silent -XPOST 'http://localhost:9200/_cluster/nodes/_local/_shutdown' > /dev/null
+	sleep 4
 
-  exit 0
+	exit 0
 }
 
 start() {
 	# launch as background process
 	/opt/elasticsearch/bin/elasticsearch -Des.config=$ELASTICSEARCH_YML &
 
-  trap shutdown SIGINT SIGTERM
+	trap shutdown SIGINT SIGTERM
 
-  wait
+	wait
 }
 
 start
